0bf617495efc9ca85555d9ac7ea6c7af2a7307d1
AlaaOsama25 <alaaosamazaki@gmail.com>
2021-09-12 00:10:31 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Movie App',
       home: Screens(),
       debugShowCheckedModeBanner: false,
     );
